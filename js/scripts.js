2bb462484fe146c67505d300d475f83a17344fb4
grinil khanna, Kory Skarbek and Shane Villars <grinil@>
2017-01-31 15:26:06 -0800

    build a funciton to pop up question - need to start workin on if else answers for correct answers.

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,23 @@
 // }
 
 
-function Question(question, image, inputSection) {
+function Question(title, question, image, label) {
+  this.title = title;
   this.question = question;
   this.image = image;
-  this.inputSection = inputSection;
+  this.questionLabel = label;
 }
 
 function Answer(answer) {
   this.answer = answer;
 }
 
-Question.prototype.puzQuestion = function () {
-  return this.question;
-  return this.image;
-}
+// Question.prototype.puzQuestion = function () {
+//   return this.title;
+//   return this.question;
+//   return this.image;
+//   return this.questionLabel;
+// }
 
 Answer.prototype.puzAnswer = function () {
   if (this.answer !== 3){
@@ -29,15 +32,10 @@ Answer.prototype.puzAnswer = function () {
   }
 }
 
-var puzzleQ1 = new Question("Question: Which number should be placed in the empty triangle?", "<br><br><img " + "src='img/numTriangle.jpg'" + ">", "<br><br>" + '<form class="puzzle1Input">' +
-  '<div class="form-group">' +
-    '<label for="answer">Blundra Your Skrappa!</label>' +
-    '<input type="text" class="form-control" id="puzzle1">' + '<br>' +
-'</div>' + '<button type="submit" name="button" class="btn btn-primary">Klubba</button>' +
-   '</form>');
+var puzzleQ1 = new Question("The Bermuda Triangle", "Question: Which number should be placed in the empty triangle?", "<img " + "src='img/numTriangle.jpg'" + ">", "Blundra Your Skrappa!");
 // var puzzleA1 = new Answer(3)
 
-var puzzleQ2 = new Question('If all Laddies are Razzies and all Razzies are Maddies, all Laddies are definitely Maddies?', "<br><br><img " + "src='img/oldPirate.png'" + ">")
+var puzzleQ2 = new Question('If all Laddies are Razzies and all Razzies are Maddies, all Laddies are definitely Maddies?', "<img " + "src='img/oldPirate.png'" + ">")
 // var puzzleAnswer4 = new Answer(BOOLEAN);
 
 
@@ -46,50 +44,65 @@ var puzzleQ2 = new Question('If all Laddies are Razzies and all Razzies are Madd
 
 
 $(document).ready(function() {
+  function puzzleQuestion(puzzleQuestion) {
+    $("#puzzle").show();
+    $("#title").text(puzzleQuestion.title);
+    console.log(puzzleQuestion.title);
+    $("#question").text(puzzleQuestion.question);
+    $("#question").append(puzzleQuestion.image);
+    $("#label").text(puzzleQuestion.questionLabel);
+    $("form").submit(function(event) {
+      event.preventDefault();
+      var userAnswer = parseInt($("#answer").val());
+      var correctAnswer = new Answer(userAnswer);
+      if (userAnswer === 3) {
+        $(".puzzle").hide();
+        console.log(correctAnswer);
+        $(".two").fadeIn();
+        $("#response").text(correctAnswer.puzAnswer)
+      }
+    });
+  }
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var answer = parseInt($("#answer").val());
+  });
 
-    if (answer === 5) {
-      $(".puzzle").hide();
-      $(".two").fadeIn();
-    } else {
-      alert('Try Again!')
-    }
-    
-  })
-    
         $(".puzzle1Input").click(function() {
     alert("I was found by JQuery");
   })
 
-  $("#puzzle1Title").click(function() {
-    $("#puzzle1Start").append(puzzleQ1.puzQuestion());
-    $("#puzzle1Start").append(puzzleQ1.image);
-    $("#puzzle1Start").append(puzzleQ1.inputSection);
-    $("form").submit(function(event) {
-      event.preventDefault();
-      var userAnswer = parseInt($("#puzzle1").val());
-      console.log($("#puzzle1").val());
-      console.log(userAnswer);
-      var correctAnswer = new Answer(userAnswer);
-      console.log(correctAnswer);
-      $("#puzzle1Answer").append(correctAnswer.puzAnswer)
-    });
-  });
+
 
   $("#puzzle2Title").click(function() {
-    $("#puzzle2Start").append(puzzleQ2.puzQuestion());
+    $("#puzzle2Start").text(puzzleQ2.puzQuestion());
     $("#puzzle2Start").append(puzzleQ2.image);
-  }); 
-  
-  
+  });
 
-  $("button#r1c1-right").click(function() {
+
+
+  $("button#r1c1-right").click(function () {
     $(".one").fadeOut();
-    $(".puzzle").show();
-  })
+    return puzzleQuestion(puzzleQ1);
+
+  });
+    // $(".one").fadeOut();
+    // $("#puzzle").show();
+    // $("#title").text(puzzleQuestion.title);
+    // $("#question").text(puzzleQuestion.question);
+    // $("#question").append(puzzleQuestion.image);
+    // $("#label").text(puzzleQuestion.questionLabel);
+    // $("form").submit(function(event) {
+    //   event.preventDefault();
+    //   var userAnswer = parseInt($("#answer").val());
+    //   var correctAnswer = new Answer(userAnswer);
+    //   if (userAnswer === 3) {
+    //     $(".puzzle").hide();
+    //     console.log(correctAnswer);
+    //     $(".two").fadeIn();
+    //     $("#response").text(correctAnswer.puzAnswer)
+    //   }
+    // });
   $("button#r1c1-down").click(function() {
     $(".one").fadeOut();
     $(".seven").fadeIn();
