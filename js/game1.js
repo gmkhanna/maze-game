dad7c6234b7fc380c78933db9f7a6e6ee9b08939
grinil khanna, Kory Skarbek and Shane Villars <grinil@>
2017-02-01 10:30:31 -0800

    add the function of movement arrow mapping

--- js/game1.js
+++ js/game1.js
@@ -3,10 +3,27 @@ $(document).ready(function(){
 
   var ctx = document.getElementById("ctx").getContext("2d");
 
+var LEFT_KEY = 37;
+var RIGHT_KEY = 39;
+var UP_KEY = 38;
+var DOWN_KEY = 40;
+
+
+
   var hero = new Object();
   hero.element = 'hero';
   hero.x = 250;
   hero.y = 710;
+
+
+var controller = new Object();
+
+  function toggleKey(keyCode, isPressed) {
+    console.log(keyCode);
+  }
+
+
+
 
   function setPosition(sprite) {
     var e = document.getElementById(sprite.element);
@@ -14,7 +31,17 @@ $(document).ready(function(){
     e.style.top = sprite.y + 'px';
   }
 
+
+document.onkeydown = function(event) {
+  toggleKey(event.keyCode, true);
+};
+
+document.onkeyup = function(event) {
+  toggleKey(event.keyCode, false);
+};
+
+
   setPosition(hero);
-  
+
 
 })
